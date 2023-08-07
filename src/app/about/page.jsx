5652116ef6d0186ccc7ee06da3417c2dd13677b8
Shubham Raut <shubhamrao12321@gmail.com>
2023-08-07 14:44:54 +0530

    pages

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,23 +1,63 @@
 import React from "react";
 import styles from "./page.module.css";
 import Image from "next/image";
-const abc = 13;
+import Button from "@/components/Button/Button";
 
-console.log(abc);
-
-export default function About() {
+const About = () => {
   return (
     <div className={styles.container}>
       <div className={styles.imgContainer}>
         <Image
-          src="https://images.unsplash.com/photo-1497681883844-82b4f0a359a4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80"
+          src="https://images.pexels.com/photos/3194521/pexels-photo-3194521.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
           fill={true}
-          alt="image yet to come"
-          className={styles.imgContainer}
+          alt=""
+          className={styles.img}
         />
-        <div className={styles.imgText}></div>
+        <div className={styles.imgText}>
+          <h1 className={styles.imgTitle}>Digital Storytellers</h1>
+          <h2 className={styles.imgDesc}>
+            Handcrafting award winning digital experiences
+          </h2>
+        </div>
       </div>
-      <div className={styles.textContainer}></div>
+      <div className={styles.textContainer}>
+        <div className={styles.item}>
+          <h1 className={styles.title}>Who Are We?</h1>
+          <p className={styles.desc}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
+            quae dolor, optio voluptatibus magnam iure esse tempora beatae. A
+            suscipit eos. Animi quibusdam cum omnis officiis voluptatum quo ea
+            eveniet? Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Ducimus quae dolor, optio voluptatibus magnam iure esse tempora
+            beatae, a suscipit eos. Animi quibusdam cum omnis officiis
+            <br />
+            <br />
+            voluptatum quo ea eveniet? Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Ducimus quae dolor, optio voluptatibus magnam iure
+            esse tempora beatae, a suscipit eos. Animi quibusdam cum omnis
+            officiis voluptatum quo ea eveniet?
+          </p>
+        </div>
+        <div className={styles.item}>
+          <h1 className={styles.title}>What We Do?</h1>
+          <p className={styles.desc}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
+            quae dolor, optio voluptatibus magnam iure esse tempora beatae, a
+            suscipit eos. Animi quibusdam cum omnis officiis voluptatum quo ea
+            eveniet? Lorem ipsum dolor sit amet consectetur adipisicing elit. -
+            Creative Illustrations
+            <br />
+            <br /> - Dynamic Websites
+            <br />
+            <br /> - Fast and Handy
+            <br />
+            <br /> - Mobile Apps
+          </p>
+          <Button url="/contact" text="Contact" />
+        </div>
+      </div>
     </div>
   );
-}
+};
+
+export default About;
--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -1,5 +1,24 @@
 import React from "react";
+import styles from "./page.module.css";
+import Link from "next/link";
 
-export default function Portfolio() {
-  return <div>Portfolio</div>;
-}
+const Portfolio = () => {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.selectTitle}>Choose a gallery</h1>
+      <div className={styles.items}>
+        <Link href="/portfolio/illustrations" className={styles.item}>
+          <span className={styles.title}>Illustrations</span>
+        </Link>
+        <Link href="/portfolio/websites" className={styles.item}>
+          <span className={styles.title}>Websites</span>
+        </Link>
+        <Link href="/portfolio/application" className={styles.item}>
+          <span className={styles.title}>Application</span>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Portfolio;
